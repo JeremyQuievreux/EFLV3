bce6258524286e1fedc4c247d69314a8e2cc44b6
Quievreux <jquievreux.devpro@gmail.com>
2022-08-16 07:19:56 +0200

    modify data architecture

---
+++ data/Calendar/FR/day_01.js
@@ -0,0 +1,169 @@
+export const fr_day_1 = [
+  {
+    id: "FR22/23-D01-M01",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "06/08/2022",
+    time: "21:00",
+    home_team: "Clermont",
+    home_score: "0",
+    home_scorers:[],
+    away_team: "PSG",
+    away_score: "5",
+    away_scorers:[
+      {
+        name: "Neymar",
+        time: "9"
+      },
+      {
+        name: "Achraf Hakimi",
+        time: "26"
+      },
+      {
+        name: "Marquinhos",
+        time: "38"
+      },
+      {
+        name: "Lionel Messi",
+        time: "80"
+      },
+      {
+        name: "Lionel Messi",
+        time: "86"
+      }
+    ],
+    finished: true,
+  },
+  {
+    id: "FR22/23-D01-M02",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "07/08/2022",
+    time: "20:45",
+    home_team: "Marseille",
+    home_score: "4",
+    home_scorers:[
+      {
+        name: "Wout Faes",
+        time: "13"
+      },
+      {
+        name: "Nuno Tavares",
+        time: "25"
+      },
+      {
+        name: "Luis Suárez",
+        time: "75"
+      },
+      {
+        name: "Luis Suárez",
+        time: "90 + 4"
+      }
+    ],
+    away_team: "Reims",
+    away_score: "1",
+    away_scorers:[
+      {
+        name: "Folarin",
+        time: "84"
+      }
+    ],
+    finished: true,
+  },
+  {
+    id: "FR22/23-D01-M03",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "05/08/2022",
+    time: "21:00",
+    home_team: "Lyon",
+    home_score: "2",
+    away_team: "Ajaccio",
+    away_score: "1",
+    finished: true,
+  },
+  {
+    id: "FR22/23-D01-M04",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "06/08/2022",
+    time: "17:00",
+    home_team: "Strasbourg",
+    home_score: "1",
+    away_team: "Monaco",
+    away_score: "2",
+    finished: true,
+  },
+  {
+    id: "FR22/23-D01-M05",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "07/08/2022",
+    time: "17:00",
+    home_team: "Rennes",
+    home_score: "0",
+    away_team: "Lorient",
+    away_score: "1",
+    finished: true,
+  },
+  {
+    id: "FR22/23-D01-M06",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "07/08/2022",
+    time: "13:00",
+    home_team: "Toulouse",
+    home_score: "1",
+    away_team: "Nice",
+    away_score: "1",
+    finished: true,
+  },
+  {
+    id: "FR22/23-D01-M07",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "07/08/2022",
+    time: "15:00",
+    home_team: "Lille",
+    home_score: "4",
+    away_team: "Auxerre",
+    away_score: "1",
+    finished: true,
+  },
+  {
+    id: "FR22/23-D01-M08",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "07/08/2022",
+    time: "15:00",
+    home_team: "Angers",
+    home_score: "0",
+    away_team: "Nantes",
+    away_score: "0",
+    finished: true,
+  },
+  {
+    id: "FR22/23-D01-M09",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "07/08/2022",
+    time: "15:00",
+    home_team: "Montpellier",
+    home_score: "3",
+    away_team: "Troyes",
+    away_score: "2",
+    finished: true,
+  },
+  {
+    id: "FR22/23-D01-M10",
+    league: "Ligue 1",
+    info: "1° Journée",
+    date: "07/08/2022",
+    time: "15:00",
+    home_team: "Lens",
+    home_score: "3",
+    away_team: "Brest",
+    away_score: "2",
+    finished: true,
+  },
+]
