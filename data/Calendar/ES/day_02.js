47303e06ccf2e9ed161c48d20aa9629a44055342
DevHÿllen <jquievreux.devpro@gmail.com>
2022-10-29 18:22:20 +0200

    add data ES 1 -> 2

--- data/Calendar/ES/day_02.js
+++ data/Calendar/ES/day_02.js
@@ -6,12 +6,17 @@ export const es_day_2 = [
     date: "21/08/2022",
     time: "",
     home_team: "Ath Bilbao",
-    home_score: "",
-    home_scorers: [],
+    home_score: "1",
+    home_scorers: [
+      {
+        name: "Berenguer",
+        time: "43",
+      },
+    ],
     away_team: "Valence",
-    away_score: "",
+    away_score: "0",
     away_scorers: [],
-    finished: false,
+    finished: true,
   },
   {
     id: "ES22/23-D02-M02",
@@ -20,12 +25,21 @@ export const es_day_2 = [
     date: "21/08/2022",
     time: "",
     home_team: "Atl Madrid",
-    home_score: "",
+    home_score: "0",
     home_scorers: [],
     away_team: "Villarreal",
-    away_score: "",
-    away_scorers: [],
-    finished: false,
+    away_score: "2",
+    away_scorers: [
+      {
+        name: "Pino",
+        time: "73",
+      },
+      {
+        name: "Gerard",
+        time: "90+7",
+      },
+    ],
+    finished: true,
   },
   {
     id: "ES22/23-D02-M03",
@@ -34,12 +48,21 @@ export const es_day_2 = [
     date: "21/08/2022",
     time: "",
     home_team: "Osasuna",
-    home_score: "",
-    home_scorers: [],
+    home_score: "2",
+    home_scorers: [
+      {
+        name: "Avila (P)",
+        time: "37",
+      },
+      {
+        name: "Kite (P)",
+        time: "79",
+      },
+    ],
     away_team: "Cadix",
-    away_score: "",
+    away_score: "0",
     away_scorers: [],
-    finished: false,
+    finished: true,
   },
   {
     id: "ES22/23-D02-M04",
@@ -48,12 +71,34 @@ export const es_day_2 = [
     date: "21/08/2022",
     time: "",
     home_team: "Celta Vigo",
-    home_score: "",
-    home_scorers: [],
+    home_score: "1",
+    home_scorers: [
+      {
+        name: "Aspas (P)",
+        time: "23",
+      },
+    ],
     away_team: "Real Madrid",
-    away_score: "",
-    away_scorers: [],
-    finished: false,
+    away_score: "4",
+    away_scorers: [
+      {
+        name: "Benzema (P)",
+        time: "14",
+      },
+      {
+        name: "Modric",
+        time: "42",
+      },
+      {
+        name: "Vinicius Jr",
+        time: "56",
+      },
+      {
+        name: "Valverde",
+        time: "66",
+      },
+    ],
+    finished: true,
   },
   {
     id: "ES22/23-D02-M05",
